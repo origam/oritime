1bffe8cb14a78d31cc311950fdb5a2ba76e2f73d
Neleesi <soptinka3@gmail.com>
2024-01-12 15:20:45 +0100

    Initial commit

---
+++ Docker/oritime.cmd
@@ -0,0 +1,2 @@
+docker run --name oritime --env-file C:\Users\David\Documents\N\Origam\OrigamProjects\Oritime\Docker\oritime.env -it -v C:\Users\David\Documents\N\Origam\OrigamProjects\Oritime:/home/origam/HTML5/data/origam -p 8080:8080 origam/server:master-latest
+
